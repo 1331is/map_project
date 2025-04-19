4fa7698cc2b7d3b154acd2747498328874a5c678
UlyaG <154477354+UlyaG@users.noreply.github.com>
2025-04-19 17:20:35 +0300

    Delete map_project directory

--- map_project/handleclick.cpp
+++
@@ -1,8 +0,0 @@
-#include "handleclick.h"
-
-handleClick::handleClick(QObject* parent) : QGraphicsScene(parent) {}
-
-void handleClick::mousePressEvent(QGraphicsSceneMouseEvent* event) {
-    QGraphicsScene::mousePressEvent(event);
-    emit sceneClicked(event->scenePos());
-}
